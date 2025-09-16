54f17b8088af949e0be21274c08f41412938e204
codeurient <codeurient@gmail.com>
2025-09-16 13:09:42 +0400

    create data

--- core/post.php
+++ core/post.php
@@ -62,9 +62,25 @@ class Post {
     }
 
 
-
-
     public function create() {
-        $query = 'INSERT INTO ' . $this-table . ' title = :title, body = :body, author = :author, category_id = :category_id,';
+        $query = 'INSERT INTO ' . $this->table . ' SET title= :title, body= :body, author= :author, category_id= :category_id';
+        $stmt = $this->conn->prepare($query);
+
+        $this->title        = htmlspecialchars(strip_tags($this->title));
+        $this->body         = htmlspecialchars(strip_tags($this->body));
+        $this->author       = htmlspecialchars(strip_tags($this->author));
+        $this->category_id  = htmlspecialchars(strip_tags($this->category_id));
+
+        $stmt->bindParam(':title',       $this->title);
+        $stmt->bindParam(':body',        $this->body);
+        $stmt->bindParam(':author',      $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+
+        if($stmt->execute()) {
+            return true;
+        }
+
+        printf("Error %s. \n", $stmt->error);
+        return false;
     }
 }
