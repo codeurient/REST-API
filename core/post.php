7d73ea7a4d1d3c3c02a7322c6baa9989ba294676
codeurient <codeurient@gmail.com>
2025-09-16 08:54:37 +0400

    single read

--- core/post.php
+++ core/post.php
@@ -36,4 +36,35 @@ class Post {
 
         return $stmt;
     }
+
+    public function read_single() {
+        $query = 'SELECT 
+        c.name as category_name,
+        p.id,
+        p.category_id,
+        p.title,
+        p.body,
+        p.author,
+        p.created_at FROM '.$this->table . ' p LEFT JOIN categories c ON p.category_id = c.id WHERE p.id = ? LIMIT 1';
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->title         = $row['title'];
+        $this->body          = $row['body'];
+        $this->author        = $row['author'];
+        $this->category_id   = $row['category_id'];
+        $this->category_name = $row['category_name'];
+
+        // return $stmt;
+    }
+
+
+
+
+    public function create() {
+        $query = 'INSERT INTO ' . $this-table . ' title = :title, body = :body, author = :author, category_id = :category_id,';
+    }
 }
