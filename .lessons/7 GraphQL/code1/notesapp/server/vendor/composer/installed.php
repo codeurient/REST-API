18d34d18d1e5bbf3432fae4c77847a103836d7c8
codeurient <codeurient@gmail.com>
2025-10-01 15:21:50 +0400

    test with postman

---
+++ .lessons/7 GraphQL/code1/notesapp/server/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '948eb7544e46cbc38d122f2aed10d3566c75192c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '948eb7544e46cbc38d122f2aed10d3566c75192c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'webonyx/graphql-php' => array(
+            'pretty_version' => 'v15.24.0',
+            'version' => '15.24.0.0',
+            'reference' => '030a04d22d52d7fc07049d0e3b683d2b40f90457',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webonyx/graphql-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
